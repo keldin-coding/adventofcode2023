b56fa597f24afbaaf9e6c6a12da6a8aecc01405e
Jonathon Anderson <coding@jpanderson.dev>
2023-12-01 13:14:14 -0800

    Finish Day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,49 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> separate_input()
+    |> Enum.map(fn line ->
+      numbers = Regex.scan(~r/(\d)/, line)
+
+      [_, first] = List.first(numbers)
+      [_, last] = List.last(numbers)
+
+      String.to_integer("#{first}#{last}")
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    mapping = %{
+      "one" => 1,
+      "two" => 2,
+      "three" => 3,
+      "four" => 4,
+      "five" => 5,
+      "six" => 6,
+      "seven" => 7,
+      "eight" => 8,
+      "nine" => 9
+    }
+
+    regex = ~r/(\d|one|two|three|four|five|six|seven|eight|nine)/
+
+    input
+    |> separate_input()
+    |> Enum.map(fn line ->
+      numbers = Regex.scan(regex, line)
+
+      [_, first] = List.first(numbers)
+      [_, last] = List.last(numbers)
+
+      String.to_integer("#{Map.get(mapping, first, first)}#{Map.get(mapping, last, last)}")
+    end)
+    |> Enum.sum()
+  end
+
+  defp separate_input(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
   end
 end
--- mix.exs
+++ mix.exs
@@ -14,13 +14,14 @@ defmodule AdventOfCode.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :inets]
+      extra_applications: [:logger, :inets, :public_key]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ssl_verify_fun, "~> 1.1.7", manager: :rebar3, override: true},
       {:benchee, "~> 1.0"}
     ]
   end
